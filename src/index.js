cbfec609bd069f10492bdef395d51707c48c4adc
we.ralston <105259322+weralston@users.noreply.github.com>
2023-05-15 13:06:54 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import WeatherSearch from "./WeatherSearch";
+
+import "./styles.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <div className="App">
+      <h1>Weather App</h1>
+      <WeatherSearch />
+    </div>
+  </StrictMode>
+);
